0586f55802e719c4df2978bcc02d46d5fb14dd5e
CeruleanSource <abdullah.mohamed@ryerson.ca>
2021-10-04 00:12:43 -0400

    local location + persistence setup

--- app/src/main/java/com/deluxe_viper/livestreamapp/MapsFragment.kt
+++ app/src/main/java/com/deluxe_viper/livestreamapp/MapsFragment.kt
@@ -1,11 +1,17 @@
 package com.deluxe_viper.livestreamapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,8 +19,13 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MapsFragment : Fragment() {
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocClient: FusedLocationProviderClient
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -26,9 +37,11 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        getCurrentLocation()
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     override fun onCreateView(
@@ -39,9 +52,72 @@ class MapsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupLocClient()
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun setupLocClient() {
+        fusedLocClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
+
+    private fun requestLocPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_LOCATION
+        )
+    }
+
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocPermissions()
+        } else {
+            fusedLocClient.lastLocation.addOnCompleteListener {
+                val location = it.result
+
+                val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                val ref: DatabaseReference = database.getReference("test")
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+
+                    map.addMarker(MarkerOptions().position(latLng).title("You are currently here!"))
+                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+
+                    map.moveCamera(update)
+
+                    ref.setValue(location) // Save the location data to the database
+                    Log.d(TAG, "getCurrentLocation: $location")
+                } else {
+                    Log.e(TAG, "No location found")
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                Log.e(TAG, "Location permission has been denied", )
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION =
+            1 // request code to identify specific permission request
+        private const val TAG = "MapsFragment"
     }
 }
